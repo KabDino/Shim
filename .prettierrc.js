6be82a6999870f6a3628c1463bf35271e469afb7
Denis <kabachuk_denis@mail.ru>
2022-05-01 23:52:09 +0300

    Fixed SH-2

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
+  endOfLine: 'auto',
   arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
   trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  requirePragma: false,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
 };
